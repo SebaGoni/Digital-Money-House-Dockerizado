f25734ff4c4f6bf904b9b5145dd73e1f2862ebd6
Sebastian Goñi <102365148+SebaGoni@users.noreply.github.com>
2024-11-06 18:33:24 -0300

    Update AccountClient.java

--- users-service/src/main/java/com/dh/DMH/usersservice/service/client/AccountClient.java
+++ users-service/src/main/java/com/dh/DMH/usersservice/service/client/AccountClient.java
@@ -6,7 +6,7 @@ import com.dh.DMH.usersservice.dto.UserAliasUpdateRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "accounts-service")
+@FeignClient(name = "accounts-service", url = "http://accounts-service:8085")
 public interface AccountClient {
 
     @PostMapping("/accounts/create")
